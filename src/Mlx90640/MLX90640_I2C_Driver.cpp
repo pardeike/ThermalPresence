ac899b4719ce4ffe499b04ea629b477c899f72bc
Andreas Pardeike <andreas@pardeike.net>
2024-10-13 22:36:42 +0200

    BROKEN upgrade to latest repo code

--- src/Mlx90640/MLX90640_I2C_Driver.cpp
+++ src/Mlx90640/MLX90640_I2C_Driver.cpp
@@ -19,13 +19,15 @@
 #include <M5Unified.h>
 #include <Wire.h>
 
-void MLX90640_I2CInit() {
+void MLX90640_I2CInit()
+{
 }
 
 // Read a number of words from startAddress. Store into Data array.
 // Returns 0 if successful, -1 if error
 int MLX90640_I2CRead(uint8_t _deviceAddress, unsigned int startAddress,
-                     unsigned int nWordsRead, uint16_t *data) {
+                     unsigned int nWordsRead, uint16_t *data)
+{
     // Caller passes number of 'unsigned ints to read', increase this to 'bytes
     // to read'
     uint16_t bytesRemaining = nWordsRead * 2;
@@ -33,29 +35,32 @@ int MLX90640_I2CRead(uint8_t _deviceAddress, unsigned int startAddress,
     // It doesn't look like sequential read works. Do we need to re-issue the
     // address command each time?
 
-    uint16_t dataSpot = 0;  // Start at beginning of array
+    uint16_t dataSpot = 0; // Start at beginning of array
 
     // Setup a series of chunked I2C_BUFFER_LENGTH byte reads
-    while (bytesRemaining > 0) {
+    while (bytesRemaining > 0)
+    {
         Wire.beginTransmission(_deviceAddress);
-        Wire.write(startAddress >> 8);         // MSB
-        Wire.write(startAddress & 0xFF);       // LSB
-        if (Wire.endTransmission(false) != 0)  // Do not release bus
+        Wire.write(startAddress >> 8);        // MSB
+        Wire.write(startAddress & 0xFF);      // LSB
+        if (Wire.endTransmission(false) != 0) // Do not release bus
         {
             Serial.println("No ack read");
-            return (0);  // Sensor did not ACK
+            return (0); // Sensor did not ACK
         }
 
         uint16_t numberOfBytesToRead = bytesRemaining;
         if (numberOfBytesToRead > I2C_BUFFER_LENGTH)
             numberOfBytesToRead = I2C_BUFFER_LENGTH;
 
-        Wire.requestFrom((uint8_t)_deviceAddress, numberOfBytesToRead);
-        if (Wire.available()) {
-            for (uint16_t x = 0; x < numberOfBytesToRead / 2; x++) {
+        Wire.requestFrom((uint8_t)_deviceAddress, (uint8_t)numberOfBytesToRead);
+        if (Wire.available())
+        {
+            for (uint16_t x = 0; x < numberOfBytesToRead / 2; x++)
+            {
                 // Store data into array
-                data[dataSpot] = Wire.read() << 8;  // MSB
-                data[dataSpot] |= Wire.read();      // LSB
+                data[dataSpot] = Wire.read() << 8; // MSB
+                data[dataSpot] |= Wire.read();     // LSB
 
                 dataSpot++;
             }
@@ -66,25 +71,28 @@ int MLX90640_I2CRead(uint8_t _deviceAddress, unsigned int startAddress,
         startAddress += numberOfBytesToRead / 2;
     }
 
-    return (0);  // Success
+    return (0); // Success
 }
 
 // Set I2C Freq, in kHz
 // MLX90640_I2CFreqSet(1000) sets frequency to 1MHz
-void MLX90640_I2CFreqSet(int freq) {
+void MLX90640_I2CFreqSet(int freq)
+{
     // i2c.frequency(1000 * freq);
     Wire.setClock((long)1000 * freq);
 }
 
 // Write two bytes to a two byte address
 int MLX90640_I2CWrite(uint8_t _deviceAddress, unsigned int writeAddress,
-                      uint16_t data) {
+                      uint16_t data)
+{
     Wire.beginTransmission((uint8_t)_deviceAddress);
-    Wire.write(writeAddress >> 8);    // MSB
-    Wire.write(writeAddress & 0xFF);  // LSB
-    Wire.write(data >> 8);            // MSB
-    Wire.write(data & 0xFF);          // LSB
-    if (Wire.endTransmission() != 0) {
+    Wire.write(writeAddress >> 8);   // MSB
+    Wire.write(writeAddress & 0xFF); // LSB
+    Wire.write(data >> 8);           // MSB
+    Wire.write(data & 0xFF);         // LSB
+    if (Wire.endTransmission() != 0)
+    {
         // Sensor did not ACK
         Serial.println("Error: Sensor did not ack");
         return (-1);
@@ -92,10 +100,11 @@ int MLX90640_I2CWrite(uint8_t _deviceAddress, unsigned int writeAddress,
 
     uint16_t dataCheck;
     MLX90640_I2CRead(_deviceAddress, writeAddress, 1, &dataCheck);
-    if (dataCheck != data) {
+    if (dataCheck != data)
+    {
         // Serial.println("The write request didn't stick");
         return -2;
     }
 
-    return (0);  // Success
+    return (0); // Success
 }
--- src/Mlx90640/interpolation.cpp
+++ src/Mlx90640/interpolation.cpp
@@ -12,32 +12,43 @@ float bicubicInterpolate(float p[], float x, float y);
 void interpolate_image(float *src, uint8_t src_rows, uint8_t src_cols,
                        float *dest, uint8_t dest_rows, uint8_t dest_cols);
 
-float get_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y) {
-    if (x < 0) x = 0;
-    if (y < 0) y = 0;
-    if (x >= cols) x = cols - 1;
-    if (y >= rows) y = rows - 1;
+float get_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y)
+{
+    if (x < 0)
+        x = 0;
+    if (y < 0)
+        y = 0;
+    if (x >= cols)
+        x = cols - 1;
+    if (y >= rows)
+        y = rows - 1;
     return p[y * cols + x];
 }
 
 void set_point(float *p, uint8_t rows, uint8_t cols, int8_t x, int8_t y,
-               float f) {
-    if ((x < 0) || (x >= cols)) return;
-    if ((y < 0) || (y >= rows)) return;
+               float f)
+{
+    if ((x < 0) || (x >= cols))
+        return;
+    if ((y < 0) || (y >= rows))
+        return;
     p[y * cols + x] = f;
 }
 
 // src is a grid src_rows * src_cols
 // dest is a pre-allocated grid, dest_rows*dest_cols
 void interpolate_image(float *src, uint8_t src_rows, uint8_t src_cols,
-                       float *dest, uint8_t dest_rows, uint8_t dest_cols) {
+                       float *dest, uint8_t dest_rows, uint8_t dest_cols)
+{
     float mu_x = (src_cols - 1.0) / (dest_cols - 1.0);
     float mu_y = (src_rows - 1.0) / (dest_rows - 1.0);
 
-    float adj_2d[16];  // matrix for storing adjacents
+    float adj_2d[16]; // matrix for storing adjacents
 
-    for (uint8_t y_idx = 0; y_idx < dest_rows; y_idx++) {
-        for (uint8_t x_idx = 0; x_idx < dest_cols; x_idx++) {
+    for (uint8_t y_idx = 0; y_idx < dest_rows; y_idx++)
+    {
+        for (uint8_t x_idx = 0; x_idx < dest_cols; x_idx++)
+        {
             float x = x_idx * mu_x;
             float y = y_idx * mu_y;
             // Serial.print("("); Serial.print(y_idx); Serial.print(", ");
@@ -53,9 +64,9 @@ void interpolate_image(float *src, uint8_t src_rows, uint8_t src_cols,
             Serial.println("]");
             */
             float frac_x =
-                x - (int)x;  // we only need the ~delta~ between the points
+                x - (int)x; // we only need the ~delta~ between the points
             float frac_y =
-                y - (int)y;  // we only need the ~delta~ between the points
+                y - (int)y; // we only need the ~delta~ between the points
             float out = bicubicInterpolate(adj_2d, frac_x, frac_y);
             // Serial.print("\tInterp: "); Serial.println(out);
             set_point(dest, dest_rows, dest_cols, x_idx, y_idx, out);
@@ -64,7 +75,8 @@ void interpolate_image(float *src, uint8_t src_rows, uint8_t src_cols,
 }
 
 // p is a list of 4 points, 2 to the left, 2 to the right
-float cubicInterpolate(float p[], float x) {
+float cubicInterpolate(float p[], float x)
+{
     float r = p[1] + (0.5 * x *
                       (p[2] - p[0] +
                        x * (2.0 * p[0] - 5.0 * p[1] + 4.0 * p[2] - p[3] +
@@ -81,18 +93,20 @@ float cubicInterpolate(float p[], float x) {
 }
 
 // p is a 16-point 4x4 array of the 2 rows & columns left/right/above/below
-float bicubicInterpolate(float p[], float x, float y) {
+float bicubicInterpolate(float p[], float x, float y)
+{
     float arr[4] = {0, 0, 0, 0};
-    arr[0]       = cubicInterpolate(p + 0, x);
-    arr[1]       = cubicInterpolate(p + 4, x);
-    arr[2]       = cubicInterpolate(p + 8, x);
-    arr[3]       = cubicInterpolate(p + 12, x);
+    arr[0] = cubicInterpolate(p + 0, x);
+    arr[1] = cubicInterpolate(p + 4, x);
+    arr[2] = cubicInterpolate(p + 8, x);
+    arr[3] = cubicInterpolate(p + 12, x);
     return cubicInterpolate(arr, y);
 }
 
 // src is rows*cols and dest is a 4-point array passed in already allocated!
 void get_adjacents_1d(float *src, float *dest, uint8_t rows, uint8_t cols,
-                      int8_t x, int8_t y) {
+                      int8_t x, int8_t y)
+{
     // Serial.print("("); Serial.print(x); Serial.print(", "); Serial.print(y);
     // Serial.println(")");
     // pick two items to the left
@@ -105,13 +119,16 @@ void get_adjacents_1d(float *src, float *dest, uint8_t rows, uint8_t cols,
 
 // src is rows*cols and dest is a 16-point array passed in already allocated!
 void get_adjacents_2d(float *src, float *dest, uint8_t rows, uint8_t cols,
-                      int8_t x, int8_t y) {
+                      int8_t x, int8_t y)
+{
     // Serial.print("("); Serial.print(x); Serial.print(", "); Serial.print(y);
     // Serial.println(")");
     float arr[4];
-    for (int8_t delta_y = -1; delta_y < 3; delta_y++) {  // -1, 0, 1, 2
-        float *row = dest + 4 * (delta_y + 1);  // index into each chunk of 4
-        for (int8_t delta_x = -1; delta_x < 3; delta_x++) {  // -1, 0, 1, 2
+    for (int8_t delta_y = -1; delta_y < 3; delta_y++)
+    {                                          // -1, 0, 1, 2
+        float *row = dest + 4 * (delta_y + 1); // index into each chunk of 4
+        for (int8_t delta_x = -1; delta_x < 3; delta_x++)
+        { // -1, 0, 1, 2
             row[delta_x + 1] =
                 get_point(src, rows, cols, x + delta_x, y + delta_y);
         }
